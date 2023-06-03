11a8e385bcb010c892db4a800258746f4e78b328
Adriano Xavier <78619884+gothboyclick@users.noreply.github.com>
2023-06-03 14:09:29 -0300

    Update grafo.py

--- grafo.py
+++ grafo.py
@@ -1,30 +1,68 @@
 class Vertice:
-    def __init__(self, nome):
-        self.__nome = nome
+
+  pass
+  #def __init__(self, nome):
+  #self.nome = nome
+
 
 class Aresta:
-    def __init__(self, partida, chegada):
-        self.partida = partida
-        self.chegada = chegada
-        self.Coordenada = (self.partida, self.chegada)
 
-class Grafo(Aresta(Aresta, chegada), Vertice(nome)):
-    def __init__(self):
-        self.arestas = [Aresta(partida, chegada)]
-        self.vertices = [Vertice(nome)]
-        self.numVertices = 0
-        self.numArestas = 0
+  def __init__(self, partida, chegada):
+    self.partida = partida
+    self.chegada = chegada
+    self.Coordenada = (self.partida, self.chegada)
 
-    def addVertice(self, vertice):
-        self.vertices.append(vertice) 
-        self.numVertices += 1
 
-    def addAresta(self, partida, chegada):
-        self.arestas.append(Aresta(partida,chegada))
-        #self.numArestas += 1
-    def PrintGrafo(self, Aresta, Vertice):
-        print("Array de aresta: ", self.arestas,", ","")
-        print("\nArray de vertices", self.vertices,", ")
+class Grafo:
+
+  def __init__(self, n):
+    self.arestas = []
+    self.vertices = [Vertice() for i in range(n)]
+
+  def addVertice(self, vertice):
+    self.vertices.append(vertice)
+
+  def addAresta(self, partida, chegada):
+    self.arestas.append(Aresta(partida, chegada))
+
+  def removerAresta(self, a):
+    self.vertices.remove(a)
+
+  def removerVertice(self, v):
+    g.vertices.remove(v)
+
+  def numAresta(self):
+    return len(self.arestas)
+
+  def numVertices(self):
+    return len(self.vertices)
+
+  def matrizAdjacencia(self):
+    nVertice = self.numVertices()
+    indice = {}
+    #criar dicionario
+    #criar um for para listar a posicao dos vertices
+    self.matriz = [[0] * nVertice for _ in range(nVertice)]
+    for (posicao, ponto) in enumerate(self.vertices):
+      indice[ponto] = posicao
+    for ares in self.arestas:
+      vP = ares.partida
+      vC = ares.chegada
+      #dicionario = {'ponto1':'linha1','ponto2':'linha2'}
+      iP = indice[vP]
+      iC = indice[vC]
+      if (iP == iC):
+        self.matriz[iP][iC] = 2
+      else:
+        self.matriz[iC][iP] = 1
+        self.matriz[iP][iC] = 1
+
+    return self.matriz
+
+  def PrintGrafo(self, Aresta, Vertice):
+    print("Array de aresta: ", self.arestas, ", ", "")
+    print("\nArray de vertices", self.vertices, ", ")
+
 
 '''    
     def ListaAdjacencia():
@@ -36,42 +74,14 @@ class Grafo(Aresta(Aresta, chegada), Vertice(nome)):
     
     def BuscaProf():
     
-    def BuscaLarg():'''
+    def BuscaLargura():'''
+''''''
+g = Grafo(4)
+v1, v2, v3, v4 = g.vertices[0], g.vertices[1], g.vertices[2], g.vertices[3]
+g.addAresta(v1, v2)
+g.addAresta(v1, v3)
+g.addAresta(v2, v4)
+g.addAresta(v3, v4)
+print(g.numAresta())
 
-'''def matrizAdjacencia(self):
-        self.dicionario = {}
-        
-        #criar dicionario
-        #criar um for para listar a posicao dos vertices
-        self.matriz = [[0]*self.numVertices for _ in range(self.numVertices)]
-        
-        for x in self.arestas:
-            vP = x.getPartida().getId()
-            vC = x.getChegada().getId()
-            self.matriz[vP][vC] += 1
-            if vP != vC:
-                self.matriz[vC][vP] += 1
-        
-        return self.matriz'''
-g = Grafo()
-g.addVertice(Vertice('v1'))
-g.addVertice(Vertice('v2'))
-g.addVertice(Vertice('v3'))
-g.addVertice(Vertice('v4'))
-g.addAresta(Aresta(g.getVertice(0),g.getVertice(1)))
-g.addAresta(Aresta(g.getVertice(0),g.getVertice(1)))
-g.addAresta(Aresta(g.getVertice(1),g.getVertice(2)))
-g.addAresta(Aresta(g.getVertice(0),g.getVertice(3)))
-g.addAresta(Aresta(g.getVertice(2),g.getVertice(3)))
-g.addAresta(Aresta(g.getVertice(2),g.getVertice(3)))
-g.addAresta(Aresta(g.getVertice(0),g.getVertice(0)))
-for x in g.matrizAdjacencia():
-    print(*x)
-
-dicionario={
-    'vertice1': '1',    
-    'vertice2': '2',
-    'vertice3': '3',
-}
-#dicionario.pop('vertice1')
-#print(dicionario)
+print(g.matrizAdjacencia())
